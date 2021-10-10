f3d354bcdc1ff704b927e44c77c7ce52ca78b8e4
Heyson <hs@heysonseverino.com>
2021-10-10 16:31:24 +0000

    add signup functionality

--- app.py
+++ app.py
@@ -24,6 +24,23 @@ def get_episode_review():
 
 @app.route("/signup", methods= ["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        # check id username already exists in database
+        exisiting_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+
+        if exisiting_user:
+            flash("Username already exists")
+            return redirect(url_for("signup"))
+
+        signup = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(signup)
+
+        # put the new user into 'session' cookie
+        session["users"] = request.form.get("username").lower()
+        flash("Sign Up Successful")
     return render_template("signup.html")
 
 
